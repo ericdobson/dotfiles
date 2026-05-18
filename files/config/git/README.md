df8a01a9e6dc17c3a9356567e9c1dd68bf693a9f
Eric Dobson <email@ericdobson.com>
2026-05-18 17:14:08 -0400

    track git config

---
+++ files/config/git/README.md
@@ -0,0 +1,5 @@
+# git
+
+## External Setup Required
+
+* Ensure signing key is [set up in GitHub](https://github.com/settings/keys).
---
+++ files/config/git/rc.zsh
@@ -0,0 +1,13 @@
+#!/usr/bin/env zsh
+
+# Sourced from zshrc. Keep this file limited to interactive shell behavior.
+alias bls='git ls'
+alias brn='git brn'
+alias cm='git commit'
+alias cma='git commit -a'
+alias co='git checkout'
+alias com='git checkout main'
+alias gd='git diff'
+alias gp='git push'
+alias gs='git status'
+alias gss='git status --short'
