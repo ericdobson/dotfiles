74f2d9574801102a07e4f0b4b630ec00dd7a9d86
Eric Dobson <email@ericdobson.com>
2026-05-18 17:12:59 -0400

    track homebrew config

---
+++ files/config/homebrew/env.zsh
@@ -0,0 +1,5 @@
+#!/usr/bin/env zsh
+
+# Sourced from zshenv. Keep this file limited to environment variables.
+export HOMEBREW_NO_ENV_HINTS=1
+export HOMEBREW_BUNDLE_FILE="$XDG_CONFIG_HOME/homebrew/Brewfile"
---
+++ files/config/homebrew/rc.zsh
@@ -0,0 +1,5 @@
+#!/usr/bin/env zsh
+
+# Sourced from zshrc. Keep this file limited to interactive shell behavior.
+alias bb="brew bundle"
+alias bf="hx $XDG_CONFIG_HOME/homebrew/Brewfile"
