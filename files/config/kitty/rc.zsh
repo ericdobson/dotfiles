b26ef811457e1ed8f5d7a12e76a1263b3ab9e201
Eric Dobson <email@ericdobson.com>
2026-05-18 17:11:47 -0400

    track kitty config

---
+++ files/config/kitty/rc.zsh
@@ -0,0 +1,5 @@
+#!/usr/bin/env zsh
+
+# Sourced from zshrc. Keep this file limited to interactive shell behavior.
+alias kc="$VISUAL $XDG_CONFIG_HOME/kitty/kitty.conf"
+alias kcd="cl; cd $XDG_CONFIG_HOME/kitty; eza -al"
