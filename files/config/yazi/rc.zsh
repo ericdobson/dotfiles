83f8f0698bc477561d3a4909636f790a8f47faa3
Eric Dobson <email@ericdobson.com>
2026-05-18 16:34:52 -0400

    track yazi config

---
+++ files/config/yazi/rc.zsh
@@ -0,0 +1,10 @@
+#!/usr/bin/env zsh
+
+# Sourced from zshrc. Keep this file limited to interactive shell behavior.
+function y() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	command yazi "$@" --cwd-file="$tmp"
+	IFS= read -r -d '' cwd < "$tmp"
+	[ "$cwd" != "$PWD" ] && [ -d "$cwd" ] && builtin cd -- "$cwd"
+	rm -f -- "$tmp"
+}
