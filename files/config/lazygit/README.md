238788ee363349552f32748b3bc61302d10340fc
Eric Dobson <email@ericdobson.com>
2026-06-09 16:43:24 -0400

    install and configure lazygit

---
+++ files/config/lazygit/README.md
@@ -0,0 +1,3 @@
+# Lazygit
+
+- [2019 lazygit tutorial](https://youtu.be/CPLdltN7wgE?si=fAgc7D6Hcby6ZY0t)
---
+++ files/config/lazygit/rc.zsh
@@ -0,0 +1,4 @@
+#!/usr/bin/env zsh
+
+# Sourced from zshrc. Keep this file limited to interactive shell behavior.
+alias lg="lazygit"
