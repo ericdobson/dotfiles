359878d9fc3ccf21faa42fd4dd1ac5d786648971
Eric Dobson <email@ericdobson.com>
2026-06-16 22:23:12 -0400

    remove incorrectly placed fastmail api token

--- files/config/codex/env.zsh
+++ files/config/codex/env.zsh
@@ -2,4 +2,3 @@
 
 # Sourced from zshenv. Keep this file limited to environment variables.
 export CODEX_HOME="$XDG_CONFIG_HOME/codex"
-export FASTMAIL_API_TOKEN="op://Private/jmtjwr7qyvosfdd5soqv754574/credential"
