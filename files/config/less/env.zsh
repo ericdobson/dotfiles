134523e1d2b55049cb6a2527c5780e3787ad5bfb
Eric Dobson <email@ericdobson.com>
2026-06-18 23:18:00 -0400

    clean up less env.zsh

--- files/config/less/env.zsh
+++ files/config/less/env.zsh
@@ -9,15 +9,18 @@
 export LESSHISTFILE="$XDG_STATE_HOME/less/lesshst"
 
 # syntax highlighting
-if lesspipe="$(command -v src-hilite-lesspipe.sh 2>/dev/null)"; then
-  export LESSOPEN="| $lesspipe %s"
+if src_hilite_lesspipe="$(command -v src-hilite-lesspipe.sh 2>/dev/null)"; then
+  export LESSOPEN="| $src_hilite_lesspipe %s"
 fi
+
+# default command flags
 export LESS=' -R -X -F '
+
 # man page colors
 export LESS_TERMCAP_mb=$'\e[6m'          # begin blinking (ever used?)
-export LESS_TERMCAP_md=$'\e[2;33m'       # begin bold
-export LESS_TERMCAP_us=$'\e[2;31m'       # begin underline
-export LESS_TERMCAP_so=$'\e[1;33;41m'    # begin standout-mode - info box (ever used?)
+export LESS_TERMCAP_md=$'\e[1;33m'       # begin bold
+export LESS_TERMCAP_us=$'\e[4;34m'       # begin underline
+export LESS_TERMCAP_so=$'\e[1;30;43m'    # begin standout-mode
 export LESS_TERMCAP_me=$'\e[m'           # end mode
 export LESS_TERMCAP_ue=$'\e[m'           # end underline
 export LESS_TERMCAP_se=$'\e[m'           # end standout-mode
