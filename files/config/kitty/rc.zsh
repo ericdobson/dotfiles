9ad3eb9056957a59d67cdf8d3e167daf4b227300
Eric Dobson <email@ericdobson.com>
2026-06-26 19:43:43 -0400

    remove unused kitty aliases

--- files/config/kitty/rc.zsh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env zsh
-
-# Sourced from zshrc. Keep this file limited to interactive shell behavior.
-# Keep network work and expensive startup logic out unless there is a concrete reason.
-
-alias kc="$VISUAL $XDG_CONFIG_HOME/kitty/kitty.conf"
-alias kcd="cl; cd $XDG_CONFIG_HOME/kitty; eza -al"
