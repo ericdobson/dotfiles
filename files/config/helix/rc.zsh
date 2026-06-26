78f846959874da2a6a9b09556a742a7dbe680521
Eric Dobson <email@ericdobson.com>
2026-06-26 19:52:49 -0400

    remove temporary vim -> helix alias

--- files/config/helix/rc.zsh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env zsh
-
-# Sourced from zshrc. Keep this file limited to interactive shell behavior.
-# Keep network work and expensive startup logic out unless there is a concrete reason.
-
-alias vim="hx"
