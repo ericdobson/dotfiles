b424dac35a6d25483bf800d3fef1993d9b20326c
Eric Dobson <email@ericdobson.com>
2026-06-26 20:02:36 -0400

    remove unused git aliases (zsh)

--- files/config/git/rc.zsh
+++ files/config/git/rc.zsh
@@ -3,12 +3,6 @@
 # Sourced from zshrc. Keep this file limited to interactive shell behavior.
 # Keep network work and expensive startup logic out unless there is a concrete reason.
 
-alias bls='git ls'
-alias brn='git brn'
-alias cm='git commit'
-alias cma='git commit -a'
-alias co='git checkout'
-alias com='git checkout main'
 alias gd='git diff'
 alias gp='git push'
 alias gs='git status'
